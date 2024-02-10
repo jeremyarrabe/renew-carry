1bcef4d25712dca5b6f05cf4e0763e6c5e83ba33
Jem <jeremyxarrabe@gmail.com>
2024-02-10 09:58:24 +0800

    created a function that saves the product in local storage

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const useLocalStorage = (key) => {
   const [localStorageItems, setLocalStorageItems] = useState([]);
@@ -26,18 +26,41 @@ export const useLocalStorage = (key) => {
 
   const addItems = (value) => {
     try {
-      const items = window.localStorage.getItem(key);
-      if (items) {
-        const items = JSON.parse(localStorage.getItem(key));
-        const newItems = [...items, ...value];
-        localStorage.setItem(key, JSON.stringify(newItems));
-        console.log(`added new item with key ${key}`);
-        setLocalStorageItems(newItems);
+      // Get key in localstorage
+      const cart = window.localStorage.getItem(key);
+      // Check if the localstorage is alread set
+      if (cart) {
+        // If cart is more than 0 it means that there is multiple items in the cart so we pass it along else if its only one we force it to be an array so we can iterate over it
+        const currentCart =
+          JSON.parse(localStorage.getItem(key)).length > 0
+            ? JSON.parse(localStorage.getItem(key))
+            : [JSON.parse(localStorage.getItem(key))];
+
+        const itemInCart = currentCart.find((item) => item.id === 1);
+        // Check if item is alread in cart and just the quantity is needed to be updated
+        if (itemInCart) {
+          const updatedCart = currentCart.map((item) =>
+            item.id === value.id ? { ...item, quantity: value.quantity } : item,
+          );
+          localStorage.setItem(key, JSON.stringify(updatedCart));
+          setLocalStorageItems(updatedCart);
+        } else {
+          // append the new item with the current one and update it.
+          const newCart = [...currentCart];
+          newCart.push({
+            id: value.id,
+            title: value.title,
+            price: value.price,
+            category: value.category,
+            quantity: value.quantity,
+          });
+          localStorage.setItem(key, JSON.stringify(newCart));
+          setLocalStorageItems(newCart);
+        }
       } else {
         window.localStorage.setItem(key, JSON.stringify(value));
-        const currentItem = window.localStorage.getItem(key);
+        const currentItem = JSON.parse(window.localStorage.getItem(key));
         setLocalStorageItems(currentItem);
-        console.log(`create new item with key ${key}`);
       }
     } catch (error) {
       console.log(error);
@@ -45,5 +68,41 @@ export const useLocalStorage = (key) => {
     }
   };
 
+  useEffect(() => {
+    const items = window.localStorage.getItem(key);
+    if (items) {
+      setLocalStorageItems(JSON.parse(items));
+    }
+  }, [key]);
+
   return { localStorageItems, addItems, getItems, deleteItems };
 };
+
+// const addItems = (value) => {
+//   try {
+//     const cart = window.localStorage.getItem(key);
+//     if (cart) {
+//       const parsedCart = JSON.parse(localStorage.getItem(key));
+//       const checkIfInCart = parsedCart.find((item) => item.id === value.id);
+//       // const newItems = [...items, ...value];
+//       // console.log(a);
+//       // localStorage.setItem(key, JSON.stringify(newItems));
+//       // console.log(`added new item with key ${key}`);
+//       // setLocalStorageItems(newItems);
+
+//       if (checkIfInCart) {
+//         console.log('yes');
+//       } else {
+//         console.log('no');
+//       }
+//     } else {
+//       window.localStorage.setItem(key, JSON.stringify(value));
+//       const currentItem = JSON.parse(window.localStorage.getItem(key));
+//       setLocalStorageItems(currentItem);
+//       console.log(`create new item with key ${key}`);
+//     }
+//   } catch (error) {
+//     console.log(error);
+//     setLocalStorageItems([]);
+//   }
+// };
